b0555c57a425e65f29d924fea3a387cc0cd0453f
Van Ba Viet <N17DCCN186@student.ptithcm.edu.vn>
2021-08-14 21:32:05 +0700

    main

--- src/main/java/com/nashtech/AssetManagement_backend/entity/AssetState.java
+++
@@ -1,9 +0,0 @@
-package com.nashtech.AssetManagement_backend.entity;
-
-public enum AssetState {
-    AVAILABLE,
-    NOT_AVAILABLE,
-    ASSIGNED,
-    WAITING_FOR_RECYCLING,
-    RECYCLED
-}
--- src/main/java/com/nashtech/AssetManagement_backend/entity/AssignmentState.java
+++
@@ -1,9 +0,0 @@
-package com.nashtech.AssetManagement_backend.entity;
-
-public enum AssignmentState {
-    ACCEPTED,
-    WAITING_FOR_ACCEPTANCE,
-    WAITING_FOR_RETURNING,
-    COMPLETED,
-    CANCELED_ASSIGN,
-}
